d41ec19b93ac6f590d12add73bfd7807af63d03c
Xichen Pan <xichen.pan@gmail.com>
2024-06-23 03:22:39 -0700

    finish button

---
+++ vite.config.js
@@ -0,0 +1,39 @@
+import path from 'path';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
+import tailwindcss from 'tailwindcss';
+export default defineConfig({
+    build: {
+        lib: {
+            entry: path.resolve(__dirname, './lib/index.ts'),
+            name: '@ppx/ui',
+            fileName: function (format) { return "index.".concat(format, ".js"); },
+        },
+        rollupOptions: {
+            external: ['react', 'react-dom', 'tailwindcss'],
+            output: {
+                globals: {
+                    react: 'React',
+                    'react-dom': 'ReactDOM',
+                    tailwindcss: 'tailwindcss',
+                },
+            },
+        },
+        sourcemap: true,
+        emptyOutDir: true,
+    },
+    plugins: [react(), dts({ rollupTypes: true })],
+    css: {
+        postcss: {
+            plugins: [tailwindcss],
+        },
+    },
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './lib'),
+            '@components': path.resolve(__dirname, './lib/components'),
+            '@pages': path.resolve(__dirname, './lib/lib'),
+        },
+    },
+});
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,40 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import path from 'path';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
+import tailwindcss from 'tailwindcss';
 
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-      '@components': path.resolve(__dirname, './src/components'),
-      '@pages': path.resolve(__dirname, './src/lib'),
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, './lib/index.ts'),
+      name: '@ppx/ui',
+      fileName: (format) => `index.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom', 'tailwindcss'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+          tailwindcss: 'tailwindcss',
+        },
+      },
+    },
+    sourcemap: true,
+    emptyOutDir: true,
+  },
+  plugins: [react(), dts({ rollupTypes: true })],
+  css: {
+    postcss: {
+      plugins: [tailwindcss],
     },
   },
-})
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './lib'),
+      '@components': path.resolve(__dirname, './lib/components'),
+      '@pages': path.resolve(__dirname, './lib/lib'),
+    },
+  },
+});
